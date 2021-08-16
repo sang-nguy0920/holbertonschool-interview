71115f288120b12c046f98495deaaf7ad84edeb9
sang-nguy0920 <2321@holbertonschool.com>
2021-08-16 15:27:11 +0000

    created dir, README.md, header file, and main files

---
+++ 0x04-sandpiles/README.md
@@ -0,0 +1,3 @@
+# 0x04-sandpiles  
+
+0-sandpiles.c - Write a function that computes the sum of two sandpiles.  
---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,11 @@
+#ifndef _SANGPILES_H_
+#define _SANGPILES_H_
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+
+#endif /* _SANGPILES_H_ */
