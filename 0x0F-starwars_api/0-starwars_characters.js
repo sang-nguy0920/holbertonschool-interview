a4ba743f307593432cb0832509fdd6ce9e90c837
sangnguyen-tul-0920 <70987304+sangnguyen-tul-0920@users.noreply.github.com>
2021-11-11 17:09:41 -0600

    added for task 0

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,1 +1,31 @@
+#!/usr/bin/node
 
+const request = require('request');
+const process = require('process');
+
+request.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/', function (err, response, body) {
+  if (err) {
+    console.log(err);
+  }
+
+  const characters = JSON.parse(body).characters;
+  const ordered = {};
+
+  const whole = JSON.parse(body);
+  for (const char of whole.characters) {
+    request.get(char, function (err, res, body) {
+      const wholeChars = JSON.parse(body);
+      if (err) {
+        console.log(err);
+      } else {
+        const characterName = wholeChars.name;
+        ordered[char] = characterName;
+      }
+      if (Object.values(ordered).length === characters.length) {
+        characters.forEach(character => {
+          console.log(ordered[character]);
+        });
+      }
+    });
+  }
+});
