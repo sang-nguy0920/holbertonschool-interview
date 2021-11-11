150f5268a133a659eb6daece667b53296a34d85d
sang-nguy0920 <2321@holbertonschool.com>
2021-11-11 21:13:13 +0000

    added fro task 0

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,1 +1,32 @@
+#!/usr/bin/node
 
+const request = require('request');
+const process = require('process');
+
+
+request.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/', function (err, response, body) {
+  if (err) {
+    console.log(err);
+  }
+
+  const characters = JSON.parse(body).characters;
+  const orderedChars = {};
+
+  const whole = JSON.parse(body);
+  for (const char of whole.characters) {
+    request.get(char, function (err, res, body) {
+      const wholeChars = JSON.parse(body);
+      if (err) {
+        console.log(err);
+      } else {
+        const characterName = wholeChars.name;
+        orderedChars[char] = characterName;
+      }
+      if (Object.values(orderedChars).length === characters.length) {
+        characters.forEach(character => {
+          console.log(orderedChars[character]);
+        });
+      }
+    });
+  }
+});
