e88652bea6aebe1254d219fbe58d65b54a2905fb
sang-nguy0920 <2321@holbertonschool.com>
2021-12-23 00:17:30 +0000

    added for task 0

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,14 @@
+#ifndef MULTI_H
+#define MULTI_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+size_t newStrlen(char *string);
+int _putchar(char c);
+int main(int argc, char **argv);
+void numCheck(int argc, char **argv);
+void *newCalloc(size_t length, size_t charSize);
+void newPrintF(char *string);
+
+#endif
