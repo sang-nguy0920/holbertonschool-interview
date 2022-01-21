4cb724245707e22b84a6a50c64bc250ad959dffa
sang-nguy0920 <2321@holbertonschool.com>
2022-01-21 01:37:44 +0000

    added files for task 0

---
+++ 0x18-merge_sort/sort.h
@@ -0,0 +1,13 @@
+#ifndef _SORT_H_
+#define _SORT_H_
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_array(const int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void split(int *array, int *new, size_t size);
+void merge(int *array, int *new, size_t size);
+
+#endif
